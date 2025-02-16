7123fb9e6c5e53007c99e02d806ddfe07326be90
João Otávio <joaootavioocortes@gmail.com>
2025-02-16 13:51:47 -0300

    Criando tela de cadastro e instalando sqlite

---
+++ UniEstoque/LoginUIs/CadastroView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UniEstoque.LoginUIs
+{
+    /// <summary>
+    /// Interaction logic for CadastroView.xaml
+    /// </summary>
+    public partial class CadastroView : Window
+    {
+        public CadastroView()
+        {
+            InitializeComponent();
+        }
+
+        private void Cadastrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IrParaLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
